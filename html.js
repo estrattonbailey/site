938804a694fb51f703e41c9b3ffaf67e36bc9fbd
estrattonbailey <ericstrattonbailey@gmail.com>
2018-10-03 17:18:00 -0400

    .

--- html.js
+++ html.js
@@ -8,12 +8,23 @@ module.exports = function (props) {
     <!DOCTYPE html>
     <html lang='en'>
       <head>
+        console.log('so nice')
         <meta charset='utf-8'>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width,initial-scale=1'>
         <link rel='canonical' href='so-nice.site'>
         ${require('html-meta-tags')(props.data)}
         <link rel='stylesheet' href='/static/main.css' />
+
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-40494652-19', 'auto');
+        ga('send', 'pageview');
+        </script>
       </head>
 
       <body>
